0431e1559d54a910998b2f2581871b9923f598dd
Piyush Kumar Gupta <piyush.24bcs10332@sst.scaler.com>
2026-03-16 18:32:50 +0530

    i m back babk

--- initial.py
+++ initial.py
@@ -4,3 +4,5 @@ print(f"CUDA Available: {torch.cuda.is_available()}")
 if torch.cuda.is_available():
     print(f"CUDA Version: {torch.version.cuda}")
     print(f"GPU Name: {torch.cuda.get_device_name(0)}")
+tensor = torch.tensor([1, 2, 3])
+print(f"Tensor: {tensor}")
